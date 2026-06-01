89761d649ee91cebeeea5395904a6e5aec580919
douzhitong <douzhitong@pwrd.com>
2026-06-01 09:59:06 +0800

    cloudflare网站比较特殊，暂时搁置

--- src/adapters/frameworks/starlight.ts
+++ src/adapters/frameworks/starlight.ts
@@ -3,7 +3,7 @@ import { createFrameworkAdapter } from './common';
 // Manual test sites:
 // 这个架构下很多网站都是子目录树，做子目录树的可以用这个框架下的网站做测试
 // - Netlify Docs, https://docs.netlify.com/ ✅ https://docs.netlify.com/deploy/deploy-notifications/页面才划了一半，但是当前页进度已经到100%了
-// - Cloudflare Developer Docs, https://developers.cloudflare.com/ ❌ 没有对嵌套链接做索引，感觉链接展开的有问题
+// - Cloudflare Developer Docs, https://developers.cloudflare.com/ ❌ 这个网站子目录跳来跳去太复杂了，后面再支持这个网站吧，现在先不处理
 // - sharp, https://sharp.pixelplumbing.com/ ✅
 export const StarlightAdapter = createFrameworkAdapter({
   id: 'framework-starlight',
